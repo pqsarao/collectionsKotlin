7281a53e43317b6c608a63260683b59201820b99
sarabatista <sara.alves@outlook.com>
2021-08-27 21:48:36 +0100

    test created

---
+++ app/src/androidTest/java/collections/TesteIntArray.kt
@@ -0,0 +1,18 @@
+package collections
+
+fun main(){
+    val values = IntArray(5)
+
+    values[0] = 1
+    values[1] = 7
+    values[2] = 6
+    values[3] = 3
+    values[4] = 2
+
+    for(valor in values){
+        println(valor)
+    }
+    values.forEach {
+        println(it)
+    }
+}
